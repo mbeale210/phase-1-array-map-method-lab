9bddfe935673c1c85f27c6185df12ac0f820f149
Mark Beale <bealebeaverst@gmail.com>
2024-07-23 02:41:00 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  const titleCasedTutorials = tutorials.map((title) => {
+    const words = title.split(" ");
+    const titleCasedWords = words.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    return titleCasedWords.join(" ");
+  });
+
+  return titleCasedTutorials;
+  return tutorials;
 }
+
+console.log(titleCased());
